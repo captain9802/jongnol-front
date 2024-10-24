7df8e4e76d2511ff93c0799ebf908f7d03785b5b
SonWuSeung <captain9802@gmail.com>
2024-10-24 17:08:59 +0900

    Style : - login.scss 추가 및 css 적용 - login.js Grid 스타일을 Grid2로 변경

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,8 +1,9 @@
 import React, { useCallback, useState } from 'react';
-import { Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
+import { Button, Container, Grid2, Link, TextField, Typography } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { login } from '../../apis/userApi';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import '../../styles/login/login.scss';
 
 const Login = () => {
     const [form, setForm] = useState({
@@ -11,10 +12,9 @@ const Login = () => {
     });
 
     const navi = useNavigate();
-
     const dispatch = useDispatch();
 
-    const textFiledchanged = useCallback((e) => {
+    const textFieldChanged = useCallback((e) => {
         setForm({
             ...form,
             [e.target.name]: e.target.value
@@ -23,66 +23,65 @@ const Login = () => {
 
     const handleLogin = useCallback((e) => {
         e.preventDefault();
-
         dispatch(login(form));
         navi("/");
     }, [form, dispatch, navi]);
 
-  return (
-    <Container component='div' maxWidth="xs" style={{marginTop: '8%'}}>
-        <form onSubmit={handleLogin}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Typography component='h1' variant='h5'>
-                        로그인
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} textAlign='right'>
-                    <TextField
-                        name='userId'
-                        variant='outlined'
-                        required
-                        id='userId'
-                        label='아이디'
-                        autoFocus
-                        fullWidth
-                        value={form.userId}
-                        onChange={textFiledchanged}
-                    ></TextField>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        name='userPw'
-                        variant='outlined'
-                        required
-                        id='userPw'
-                        label='비밀번호'
-                        fullWidth
-                        type='password'
-                        value={form.userPw}
-                        onChange={textFiledchanged}
-                    ></TextField>
-                </Grid>
-                <Grid item xs={12}>
-                    <Button
-                        type='submit'
-                        fullWidth
-                        variant='contained'
-                        color='primary'>
-                        로그인
-                    </Button>
-                </Grid>
-            </Grid>
-            <Grid container justifyContent='flex-end'>
-                <Grid item>
-                    <Link href="/join" variant='body2'>
-                       계정이 없으시면 회원가입하세요.
-                    </Link>
-                </Grid>
-            </Grid>
-        </form>
-    </Container>
-  );
+    return (
+        <Container component='div' maxWidth="xs" style={{ marginTop: '8%' }} className='login'>
+            <form onSubmit={handleLogin}>
+                <Grid2 container spacing={2} sx={{ display: 'grid'}}>
+                    <Grid2 >
+                        <Typography component='h1' variant='h5' textAlign='center'>
+                            로그인
+                        </Typography>
+                    </Grid2>
+                    <Grid2>
+                        <TextField
+                            name='userId'
+                            variant='outlined'
+                            required
+                            id='userId'
+                            label='아이디'
+                            autoFocus
+                            fullWidth
+                            value={form.userId}
+                            onChange={textFieldChanged}
+                        />
+                    </Grid2>
+                    <Grid2>
+                        <TextField
+                            name='userPw'
+                            variant='outlined'
+                            required
+                            id='userPw'
+                            label='비밀번호'
+                            fullWidth
+                            type='password'
+                            value={form.userPw}
+                            onChange={textFieldChanged}
+                        />
+                    </Grid2>
+                    <Grid2>
+                        <Button
+                            type='submit'
+                            fullWidth
+                            variant='contained'
+                            color='primary'>
+                            로그인
+                        </Button>
+                    </Grid2>
+                </Grid2>
+                <Grid2 container justifyContent='flex-end'>
+                    <Grid2 item>
+                        <Link href="/join" variant='body2'>
+                            계정이 없으시면 회원가입하세요.
+                        </Link>
+                    </Grid2>
+                </Grid2>
+            </form>
+        </Container>
+    );
 }
 
 export default Login;
